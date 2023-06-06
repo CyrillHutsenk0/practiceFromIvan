117e4d1c683a46d77a83bc0103e13399a52ce95a
CyrillHutsenk0 <133814816+CyrillHutsenk0@users.noreply.github.com>
2023-06-07 02:01:43 +0300

    change palindrom

--- find_palindrom_words.py
+++ find_palindrom_words.py
@@ -1,9 +1,14 @@
-con = input("Enter a sentence: ")
+con = str(input("Enter a sentence: "))
+
+while len(con) <= 0:
+    con = str(input("Enter a sentence: "))
 
 
 def palindrom(sentence):
     structure = sentence.lower().split()
-    rezalt = ''
+    rezalt = []
+    newlist = []
+    dublicate = []
     for elem in structure:
         elem_list = list(elem)
         copy_el_ls = elem_list.copy()
@@ -11,10 +16,15 @@ def palindrom(sentence):
         if elem_list == copy_el_ls:
             str_elem = elem_list
             convert_el_ls = ''.join(map(str, str_elem))
-            rezalt += ' ' + convert_el_ls
+            rezalt.append(convert_el_ls)
     if bool(rezalt) is False:
         return print("We dont't found polidrom")
-    return print(f"We found polindrom: {rezalt}")
+    for i in rezalt:
+        if i not in newlist:
+            newlist.append(i)
+        else:
+            dublicate.append(i)
+    return print(f"We found polindrom: {newlist}")
 
 
 palindrom(con)
